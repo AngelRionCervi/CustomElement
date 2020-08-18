861af755852f0c10a8484d71b49d6bb61a5b5a17
AngelRionCervi <a.rioncervi@gmail.com>
2020-08-18 22:18:51 +0200

    init

---
+++ main.js
@@ -0,0 +1,124 @@
+import CustomElement from "./lib/CustomElement.js";
+
+class subEl extends CustomElement {
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        this.state = {
+            is: "ok",
+        };
+
+        shadowRoot.innerHTML = `<div data-bind="state.is"></div>`;
+    }
+
+    onInit() {
+        
+        //console.log(this.dispatchEvent)
+    }
+
+    onRender() {
+        this.props.sayHi();
+        //console.log(this.getRootNode().host.shadowRoot.innerHTML)
+    }
+}
+
+customElements.define("sub-el", subEl);
+
+export class DemoElement extends CustomElement {
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        this.state = {
+            title: "lul",
+            user: {
+                class: "color-red",
+                name: "A name",
+                lastname: "rc",
+                address: {
+                    city: "A city",
+                },
+            },
+            pets: ['<sub-el props="sayHi pets=state.pets"></sub-el>', '<sub-el props="sayHi pets=state.pets"></sub-el>', "fish"],
+        };
+
+        shadowRoot.innerHTML = `
+        <style>
+            .color-red {
+                color: red;
+            }
+            .underline {
+                text-decoration: underline;
+            }
+            .text-big {
+                font-size: 5em;
+            }
+        </style>
+      <template id="template">
+        <p id="template-content">This is the content of the template</p>
+      </template>
+      
+      <div id="container">
+        <h1>hi <span data-bind="state.user.name" ></span></h1>
+        <h3 data-loop="item of state.pets"><div><p loop-item>hey</p></div><p>you</p></h3>
+        <p data-bind="state.title" id="title"></p>
+        
+        <h3>user.name</h3>
+        <h2 data-bind="state.user.name" class-bind="state.user.class" class="underline random-class" id="name"></h2>
+        
+        <h3>user.address.city</h3>
+        <p on-mouseover="hoveredP" data-bind="state.user.address.city" id="city"></p>
+        
+        <h3 ref="h3title" on-click="clickedH3" data-bind="state.title"></h3>
+        <sub-el props="sayHi pets=state.pets"></sub-el>
+        <form on-submit="submitthatform">
+            <input type="checkbox" name="yes">
+            <button type="submit">send</button>
+        </form>
+      </div>
+        `;
+    }
+
+    onInit() {
+        
+    }
+
+    onRender() {
+        this.setStateTitle("TITLKE");
+        this.setStateUserAddressCity("LA");
+        this.setStateUserName("wadaheck");
+        this.setStateUserName("wadaheck2");
+        this.setStateUserClass("text-big");
+        this.setState(["state.user.name", "srmkhgzs"], ["state.user.class", "yes yes class"]);
+    }
+
+    sayHi() {
+        console.log("hi");
+    }
+
+    clickedH3(evt) {
+        evt.preventDefault();
+        console.log("wadup", evt);
+    }
+
+    hoveredP(evt) {
+        evt.preventDefault();
+        console.log("hover", evt);
+    }
+
+    submitthatform(evt) {
+        evt.preventDefault();
+        console.log("submit", Object.fromEntries(new FormData(evt.target)))
+    }
+
+    downdown(evt) {
+        console.log("mousedown sub-el", evt)
+    }
+
+    upup(evt) {
+        console.log("mouseup sub-el", evt)
+        console.log(this.refs)
+    }
+}
+
+customElements.define("demo-element", DemoElement);
