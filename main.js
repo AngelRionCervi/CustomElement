ebf80103313c2be74d536b68add0a23c1a69b86a
AngelRionCervi <a.rioncervi@gmail.com>
2020-08-21 00:31:02 +0200

    added multi class swap

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ export class DemoElement extends CustomElement {
         const shadowRoot = this.attachShadow({ mode: "open" });
         this.state = {
             showCart: false,
+            displayName: true,
+            displayTitle: true,
             title: "lul",
             user: {
                 class: "color-red",
@@ -40,6 +42,7 @@ export class DemoElement extends CustomElement {
                     city: "A city",
                 },
             },
+            rclass: "rezrzrzr",
             pets: ['<sub-el props="sayHi pets=state.pets"></sub-el>', '<sub-el props="sayHi pets=state.pets"></sub-el>', "fish"],
         };
 
@@ -54,20 +57,23 @@ export class DemoElement extends CustomElement {
             .text-big {
                 font-size: 5em;
             }
+            .dtitle {
+                font-size: 500%;
+            }
         </style>
       <template id="template">
         <p id="template-content">This is the content of the template</p>
       </template>
       
       <div id="container">
-        <h1>hi <span data-bind="state.user.name" ></span></h1>
+        <h1>hi <span data-bind="state.displayName : state.user.name" ></span></h1>
         <h3 loop="item of state.pets"><div><p loop-item>hey</p></div><p data-bind="state.title">you</p></h3>
         <p data-bind="state.title" id="title"></p>
         
         <h3>user.name</h3>
-        <h2 data-bind="state.user.name" class-bind="state.user.class" class="underline random-class" id="name"></h2>
+        <h2 data-bind="state.user.name" class-bind="state.displayName : color-red, state.displayTitle: dtitle, state.rclass" class="underline random-class" id="name"></h2>
         
-        <h3>user.address.city</h3>
+        <h3 class-bind="user.class">user.address.city</h3>
         <p on-mouseover="hoveredP" data-bind="state.user.address.city" id="city"></p>
         
         <h3 ref="h3title" on-click="clickedH3" data-bind="state.title"></h3>
@@ -81,10 +87,6 @@ export class DemoElement extends CustomElement {
         `;
     }
 
-    onInit() {
-        
-    }
-
     onRender() {
         /*
         this.setStateTitle("TITLKE");
@@ -92,7 +94,10 @@ export class DemoElement extends CustomElement {
         this.setStateUserName("wadaheck");
         this.setStateUserName("wadaheck2");
         this.setStateUserClass("text-big");*/
-        this.setState(["state.user.name", "srmkhgzs"], ["state.user.class", "yes yes class"]);
+        //this.setState(["state.displayName", true]);
+        this.setState(["state.displayTitle", true], ["state.displayName", false]);
+        this.setState("state.displayName", true)
+        this.setState("state.user.name", "exdeexd")
     }
 
     sayHi() {
