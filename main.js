26803fb9042174f6165ffea850ac6cdc39b060eb
AngelRionCervi <a.rioncervi@gmail.com>
2020-08-21 02:21:58 +0200

    merging with master

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ export class DemoElement extends CustomElement {
                 },
             },
             rclass: "rezrzrzr",
-            pets: ['<sub-el props="sayHi pets=state.pets"></sub-el>', '<sub-el props="sayHi pets=state.pets"></sub-el>', "fish"],
+            pets: ['<sub-el props="sayHi pets=pets"></sub-el>', '<sub-el props="sayHi pets=pets"></sub-el>', "fish"],
         };
 
         shadowRoot.innerHTML = `
@@ -66,23 +66,23 @@ export class DemoElement extends CustomElement {
       </template>
       
       <div id="container">
-        <h1>hi <span data-bind="state.displayName : state.user.name" ></span></h1>
-        <h3 loop="item of state.pets"><div><p loop-item>hey</p></div><p data-bind="state.title">you</p></h3>
-        <p data-bind="state.title" id="title"></p>
+        <h1>hi <span data-bind="displayName : user.name" ></span></h1>
+        <h3 loop="item of pets"><div><p loop-item>hey</p></div><p data-bind="title">you</p></h3>
+        <p data-bind="title" id="title"></p>
         
         <h3>user.name</h3>
-        <h2 data-bind="state.user.name" class-bind="state.displayName : color-red, state.displayTitle: dtitle, state.rclass" class="underline random-class" id="name"></h2>
+        <h2 data-bind="user.name" class-bind="displayName : color-red, displayTitle: dtitle, rclass" class="underline random-class" id="name"></h2>
         
         <h3 class-bind="user.class">user.address.city</h3>
-        <p on-mouseover="hoveredP" data-bind="state.user.address.city" id="city"></p>
+        <p on-mouseover="hoveredP" data-bind="user.address.city" id="city"></p>
         
-        <h3 ref="h3title" on-click="clickedH3" data-bind="state.title"></h3>
-        <sub-el props="sayHi pets=state.pets"></sub-el>
+        <h3 ref="h3title" on-click="clickedH3" data-bind="title"></h3>
+        <sub-el props="sayHi pets=pets"></sub-el>
         <form on-submit="submitthatform">
             <input type="checkbox" name="yes">
             <button type="submit">send</button>
         </form>
-        <div if-bind="state.showCart">cart<div>items : 3</div></div>
+        <div if-bind="showCart">cart<div>items : 3</div></div>
       </div>
         `;
     }
@@ -95,9 +95,11 @@ export class DemoElement extends CustomElement {
         this.setStateUserName("wadaheck2");
         this.setStateUserClass("text-big");*/
         //this.setState(["state.displayName", true]);
-        this.setState(["state.displayTitle", true], ["state.displayName", false]);
-        this.setState("state.displayName", true)
-        this.setState("state.user.name", "exdeexd")
+        this.setState(["displayTitle", true], ["displayName", false]);
+        this.setState("user.name", "exdeexd")
+        for (const o of this) {
+            console.log(o)
+        }
     }
 
     sayHi() {
